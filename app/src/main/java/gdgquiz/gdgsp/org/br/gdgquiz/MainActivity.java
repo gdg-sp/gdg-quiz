4793d12c5123a0040ea0544225f6c88a2c4730d2
Paulo Fernandes <paulofernandesjr@gmail.com>
2014-07-24 19:52:12 -0300

    clique no botao

--- app/src/main/java/gdgquiz/gdgsp/org/br/gdgquiz/MainActivity.java
+++ app/src/main/java/gdgquiz/gdgsp/org/br/gdgquiz/MainActivity.java
@@ -1,17 +1,49 @@
 package gdgquiz.gdgsp.org.br.gdgquiz;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+    private Button btnPlay;
+    private static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = getApplicationContext();
+
+                btnPlay = (Button) findViewById(R.id.btnPllay);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+
+
+
+
+
+            }
+        });
+
+
+    }
+
+    public static void showMessage(String message){
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        Log.w("GDG-QUIZ", message);
     }
 
 }
